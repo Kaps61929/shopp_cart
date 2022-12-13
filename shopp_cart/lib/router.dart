31fa1466120789dbbce0f068bc5ab4342597741b
krish <kaps61929>
2022-12-13 23:50:39 +0530

    second

---
+++ shopp_cart/lib/router.dart
@@ -0,0 +1,22 @@
+import 'dart:ffi';
+
+import 'package:flutter/material.dart';
+import 'package:shopp_cart/features/auth/screens/auth_screen.dart';
+
+Route<dynamic> generateRoute(RouteSettings routesettings) {
+  switch (routesettings.name) {
+    case AuthScreen.routname:
+      return MaterialPageRoute(
+        settings: routesettings,
+        builder: (context) => const AuthScreen(),
+      );
+
+    default:
+      return MaterialPageRoute(
+        settings: routesettings,
+        builder: (context) => const Scaffold(
+          body: Center(child: Text("Error Page doesnot exist")),
+        ),
+      );
+  }
+}
